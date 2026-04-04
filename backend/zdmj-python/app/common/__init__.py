4279a7f48b2a8816ffcab9c2f131d56243c1dad6
ycc250303 <yin2533@outlook.com>
2026-04-05 04:35:26 +0800

    refactor：下线python代码服务

--- backend/zdmj-python/app/common/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-通用模块
-包含请求/响应模型与异常处理中间件
-"""
-from app.common.request import ApiRequest
-from app.common.response import ApiResponse
-from app.common import exception_handler
-
-__all__ = ["ApiRequest", "ApiResponse", "exception_handler"]
